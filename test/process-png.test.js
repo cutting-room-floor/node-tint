ef76c1411feb2faf22964f35274461efa098b188
Dane Springmeyer <dane@dbsgeo.com>
2012-11-13 18:40:50 -0800

    fix tabs in test file

--- test/process-png.test.js
+++ test/process-png.test.js
@@ -5,25 +5,25 @@ var tint = require('..');
 
 describe('Invalid png file', function() {
     var tests = [
-    	{ file: 'small', message: 'Image size is too small'},
-    	{ file: 'lorem', message: 'Image is not a PNG file'},
-    	{ file: 'truecolour', message: 'Image does not have a palette'},
-    	{ file: 'bad-profile', message: 'Image has invalid chunk with length 0'}
+        { file: 'small', message: 'Image size is too small'},
+        { file: 'lorem', message: 'Image is not a PNG file'},
+        { file: 'truecolour', message: 'Image does not have a palette'},
+        { file: 'bad-profile', message: 'Image has invalid chunk with length 0'}
     ];
 
     tests.forEach(function(t) {
-    	var source = fs.readFileSync(path.join('./test/source', t.file +'.png'));
+        var source = fs.readFileSync(path.join('./test/source', t.file +'.png'));
 
-	it(t.file, function() {
-	    var err;
-	    try {
-		tint(source, {});
-	    }
-	    catch(e) {
-		err = e;
-	    }
-	    assert.notEqual(err, null);
-	    assert.equal(err.message, t.message);
-	});
+        it(t.file, function() {
+            var err;
+            try {
+                tint(source, {});
+            }
+            catch(e) {
+                err = e;
+            }
+            assert.notEqual(err, null);
+            assert.equal(err.message, t.message);
+        });
     });
 });
